acc343983201c407f29bebd93106e096dd7b28cb
NathanRomike <nathanromike@gmail.com>
2016-07-21 23:58:46 -0700

    Initial commit

---
+++ app/src/main/java/comnathanromike/httpsgithub/yes_you_can/MainActivity.java
@@ -0,0 +1,13 @@
+package comnathanromike.httpsgithub.yes_you_can;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/comnathanromike/httpsgithub/yes_you_can/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package comnathanromike.httpsgithub.yes_you_can;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
