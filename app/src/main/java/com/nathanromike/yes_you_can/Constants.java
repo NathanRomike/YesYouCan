0b3a11e12f603b5bd8dc61420e4429b281b58c6a
NathanRomike <nathanromike@gmail.com>
2016-07-24 16:21:20 -0700

    Remove unofficial instructables api from project.

--- app/src/main/java/com/nathanromike/yes_you_can/Constants.java
+++ app/src/main/java/com/nathanromike/yes_you_can/Constants.java
@@ -1,5 +1,4 @@
 package com.nathanromike.yes_you_can;
 
 public class Constants {
-    public static final String MASHAPE_KEY = BuildConfig.MASHAPE_KEY;
 }
--- app/src/main/java/com/nathanromike/yes_you_can/ui/CategoryActivity.java
+++ app/src/main/java/com/nathanromike/yes_you_can/ui/CategoryActivity.java
@@ -2,8 +2,16 @@ package com.nathanromike.yes_you_can.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.nathanromike.yes_you_can.Constants;
 import com.nathanromike.yes_you_can.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -12,5 +20,4 @@ public class CategoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
     }
-
 }
