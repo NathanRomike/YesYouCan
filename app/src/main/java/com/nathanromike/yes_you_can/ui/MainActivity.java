9d88321847ec9fe4d6b9ca74581145541b6ed59f
NathanRomike <nathanromike@gmail.com>
2016-07-27 13:49:05 -0700

    Add network connection validator to main button.

--- app/src/main/java/com/nathanromike/yes_you_can/ui/MainActivity.java
+++ app/src/main/java/com/nathanromike/yes_you_can/ui/MainActivity.java
@@ -1,14 +1,20 @@
 package com.nathanromike.yes_you_can.ui;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.nathanromike.yes_you_can.R;
+
+import java.net.InetAddress;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.button) Button button;
@@ -24,11 +30,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                Intent intent = new Intent(MainActivity.this, CardListsActivity.class);
-                startActivity(intent);
-                break;
+        if (isInternetAvailable()) {
+            Intent intent = new Intent(MainActivity.this, CardListsActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Please check your internet connection and try again!", Toast.LENGTH_LONG).show();
         }
+    }
+
+    private boolean isInternetAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return  networkInfo != null && networkInfo.isConnectedOrConnecting();
     }
 }
